b150a3ed9970354f097a3c4588d0c6aab0a5264b
Tolga <tolga.demirci@themarkagency.com.au>
2022-12-22 08:28:53 +1100

    image change

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,10 +3,10 @@
     class="relative flex min-h-screen items-center justify-center overflow-x-hidden bg-black font-sans"
   >
     <div class="absolute inset-0 h-full w-full">
-      <img src="/bg2.jpg" alt="" class="h-full w-full object-cover" />
+      <img src="/bg2.png" alt="" class="h-full w-full object-cover" />
     </div>
     <div
-      class="relative z-10 mx-auto max-w-5xl text-center !font-cursive text-9xl uppercase opacity-80 xl:text-[10rem]"
+      class="relative z-10 mx-auto max-w-5xl text-center !font-cursive text-9xl uppercase text-yellow-100 xl:text-[10rem]"
     >
       Take Over Has Begun
     </div>
