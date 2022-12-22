a817ca1792bb8515f2310d1e4d860a4b161197a0
Tolga <tolga.demirci@themarkagency.com.au>
2022-12-22 12:42:08 +1100

    changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,9 +6,17 @@
       <img src="/bg2.png" alt="" class="h-full w-full object-cover" />
     </div>
     <div
-      class="relative z-10 mx-auto max-w-5xl text-center !font-cursive text-9xl uppercase text-yellow-100 xl:text-[10rem]"
+      class="outline relative z-10 mx-auto max-w-5xl text-center !font-cursive text-7xl uppercase md:text-8xl xl:text-9xl xl:text-[10rem]"
     >
       Take Over Has Begun
     </div>
   </TemplateDefault>
 </template>
+<style>
+.outline {
+  color: black;
+  -webkit-text-fill-color: #faf6a4; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 5px;
+  -webkit-text-stroke-color: black;
+}
+</style>
